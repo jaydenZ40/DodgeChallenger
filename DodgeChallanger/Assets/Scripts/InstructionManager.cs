b30e68269c4e17d401cd6e7a7d4fbb8b3b43e5bf
JaydenZ <zjp981310647@gmail.com>
2019-06-10 12:52:51 -0700

    added instruction

---
+++ DodgeChallanger/Assets/Scripts/InstructionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InstructionManager : MonoBehaviour
+{
+    public TextMeshProUGUI instruction;
+
+    void Start()
+    {
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        string str = "Welcom to the battleground!" +
+            "\n\nAttacker and dodger will be switched for each round." +
+            "\n\nSuccessfully making damage will gain gold." +
+            "\n\nGold can be spent for upgrading new weapons." +
+            "\n\nYour goal is to beat your opponent." +
+            "\n\nGood luck!";
+        
+        int i = 0;
+        instruction.text = "";
+        while (i < str.Length)
+        {
+            instruction.text += str[i++];
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+}
